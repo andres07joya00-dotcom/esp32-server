5b428052921ec947e1194b832643f76282004cf0
Andres Joya <andres07joya00@gmail.com>
2026-03-31 17:17:47 -0500

    serivdor.js

--- server.js
+++ server.js
@@ -1,30 +1,58 @@
-const mqtt = require("mqtt");
 const express = require("express");
+const mqtt = require("mqtt");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+// 🔹 Servir HTML
+app.use(express.static(path.join(__dirname, "public")));
+
+// 🔹 MQTT
 const client = mqtt.connect("mqtt://broker.hivemq.com");
 
+let humedad = 0;
+
 client.on("connect", () => {
-  console.log("MQTT conectado");
+  console.log("Conectado a MQTT");
+  client.subscribe("esp32/humedad");
 });
 
-// Rutas
-app.get("/on", (req, res) => {
-  client.publish("esp32/led", "ON");
-  console.log("LED ENCENDIDO");
-  res.send("LED ENCENDIDO");
+client.on("message", (topic, message) => {
+  if (topic === "esp32/humedad") {
+    humedad = message.toString();
+    console.log("Humedad:", humedad);
+  }
 });
 
-app.get("/off", (req, res) => {
-  client.publish("esp32/led", "OFF");
-  console.log("LED APAGADO");
-  res.send("LED APAGADO");
+// 🔹 Enviar datos al HTML
+app.get("/datos", (req, res) => {
+  res.json({
+    suelo: humedad,
+    aire: "--",
+    temp: "--",
+    luz: "--"
+  });
 });
 
-// Servir la página web
-app.use(express.static(__dirname));
+// 🔹 Controlar dispositivos
+app.post("/bomba", (req, res) => {
+  client.publish("esp32/bomba", "ON");
+  res.send("Bomba ON");
+});
 
-// Levantar servidor
+app.post("/ventilador", (req, res) => {
+  client.publish("esp32/ventilador", "ON");
+  res.send("Ventilador ON");
+});
+
+app.post("/luces", (req, res) => {
+  client.publish("esp32/luces", "ON");
+  res.send("Luces ON");
+});
+
 app.listen(3000, () => {
   console.log("Servidor en http://localhost:3000");
 });
